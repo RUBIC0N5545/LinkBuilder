596e8df8e0023aa2e7bef70e0b982c20594a3f29
Dmytro Kovaliak <d.kovaliak@MacBook-Air-Dmytro.local>
2025-06-24 19:02:31 +0200

    v1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import MainPage from '../src/pages/MainPage';
 import SettingsPage from '../src/pages/SettingsPage';
 import Layout from '../src/components/Layout';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<MainPage />} />
-          <Route path="" element={<MainPage />} />
           <Route path="settings" element={<SettingsPage />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
